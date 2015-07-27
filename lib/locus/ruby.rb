38c471af42fdcd3d9312232545cb304735ffb934
dphaener <dphaener@gmail.com>
2015-07-27 09:54:37 -0700

    Initial commit

---
+++ lib/locus/ruby.rb
@@ -0,0 +1,7 @@
+require "locus/ruby/version"
+
+module Locus
+  module Ruby
+    # Your code goes here...
+  end
+end
---
+++ test/test_helper.rb
@@ -0,0 +1,4 @@
+$LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
+require 'locus/ruby'
+
+require 'minitest/autorun'
